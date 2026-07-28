94b14d3677b4f44a8f2377f99957476625066da8
sauravkrkrkr <sauravkrkrkr@gmail.com>
2026-07-29 02:12:49 +0530

    Printing value of 1D and 2D array

---
+++ app.py
@@ -0,0 +1,15 @@
+# First of all, we need to import the necessary libraries and modules that will be used in our application. This includes libraries for data manipulation, visualization, and any other specific tools required for our analytics tasks.
+# write command "pip install numpy pandas" to install the libraries.
+
+import numpy as np
+
+#Creating a simple 1D array
+scores = np.array([85, 90, 78, 92, 88])
+print(scores)
+print(type(scores))  # Output: <class 'numpy.ndarray'>
+
+# A 2D array : 3 students, 2 subjects each
+marks = np.array([
+    [85, 90], [78, 92], [88, 76]
+])
+print(marks)
