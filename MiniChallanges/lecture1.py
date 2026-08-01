395742e99cdbb66e3618683ede02cb4abf5e786e
sauravkrkrkr <sauravkrkrkr@gmail.com>
2026-08-01 15:40:18 +0530

    three miniProjects added

---
+++ MiniChallanges/lecture1.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+#Print the dataset shape
+rd = pd.read_csv("../resources/employees.csv")
+#print("Shape of the employee file is :", rd.shape)
+
+#Show the last 8 rows
+#print("The last 8 rows are: ", rd.tail(8))
+
+#Employee_Name & City for first 10 rows
+print(rd[["Employee_Name", "City"]].head(10)) #1st method
+print(rd.loc[0:9, ["Employee_Name", "City"]]) #2nd method
