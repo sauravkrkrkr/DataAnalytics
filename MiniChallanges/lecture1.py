d49ef28d43b1489f0321851843f6afadb42609bc
sauravkrkrkr <sauravkrkrkr@gmail.com>
2026-08-02 14:50:17 +0530

    Output added to the files in minichallanges

--- MiniChallanges/lecture1.py
+++ MiniChallanges/lecture1.py
@@ -4,16 +4,54 @@ import pandas as pd
 #Print the dataset shape
 rd = pd.read_csv("../resources/employees.csv")
 print("Shape of the employee file is :", rd.shape)
+#Shape of the employee file is : (120, 10)
 
 #Show the last 8 rows
 print("The last 8 rows are: ", rd.tail(8))
+''' Output:
+The last 8 rows are:      Employee_ID   Employee_Name  Department  Age  Experience  Salary  Performance_Rating  Projects       City Joining_Date
+112      EMP113  Rahul Malhotra  Operations   26           3   49000                   4         2  Ahmedabad   2023-09-08
+113      EMP114      Saanvi Das   Marketing   24           2   53500                   1         1  Ahmedabad   2024-07-14
+114      EMP115  Vishal Rathore  Operations   28           7   57500                   2         6    Lucknow   2019-03-03
+115      EMP116    Vikram Ghosh          IT   39          18  111500                   3        10     Jaipur   2015-05-15
+116      EMP117   Krishna Reddy   Marketing   49           3   54500                   5         9     Indore   2023-04-14
+117      EMP118    Swati Sharma       Sales   36          13   76000                   4        12      Delhi   2015-06-14
+118      EMP119   Preeti Kapoor     Finance   38          11   77000                   2         2      Delhi   2015-02-03
+119      EMP120  Abhishek Desai       Sales   28           5   55000                   3         9     Jaipur   2021-06-22
+'''
 
 #Employee_Name & City for first 10 rows
 print(rd[["Employee_Name", "City"]].head(10)) #1st method
 print(rd.loc[0:9, ["Employee_Name", "City"]]) #2nd method
+''' Output:
+      Employee_Name        City
+0        Diya Verma      Mumbai
+1  Kritika Malhotra      Jaipur
+2       Suresh Iyer   Hyderabad
+3     Ananya Tiwari     Lucknow
+4       Rahul Sinha  Chandigarh
+5     Manisha Gupta       Delhi
+6      Vihaan Patel     Chennai
+7     Vikram Chopra      Jaipur
+8     Anushka Verma      Indore
+9     Sanjay Tiwari  Chandigarh
+'''
 
 #iloc for the employee at row 25
 print(rd.iloc[24])
+'''Output:
+Employee_ID               EMP025
+Employee_Name         Amit Singh
+Department                 Sales
+Age                           57
+Experience                    26
+Salary                     89500
+Performance_Rating             3
+Projects                       7
+City                     Lucknow
+Joining_Date          2015-10-21
+Name: 24, dtype: object'''
 
 #Find the maximum Salary
 print("The max salary is : ", rd["Salary"].max())
+#The max salary is :  127000
--- MiniChallanges/lecture1HW.py
+++ MiniChallanges/lecture1HW.py
@@ -10,11 +10,40 @@ It also shows that how many not-null values are present in a particular column.
 If we need to check the number of null values then subtract not-null values from the total number of rows.'''
 
 #loc for 3 columns, rows 10-20
-#print(df.columns)
-#print(df.loc[10:20, ["Employee_ID","Employee_Name", "Department"]])
+print(df.columns) #If columns name not known, then it will fetch from the file and print the column names.
+print(df.loc[10:20, ["Employee_ID","Employee_Name", "Department"]])
+'''Output:
+   Employee_ID     Employee_Name  Department
+10      EMP011      Rekha Chopra       Sales
+11      EMP012      Nikhil Sinha          IT
+12      EMP013    Kritika Sharma  Operations
+13      EMP014       Riya Saxena     Finance
+14      EMP015       Dhruv Joshi   Marketing
+15      EMP016      Priya Bansal  Operations
+16      EMP017       Dhruv Mehta          HR
+17      EMP018  Rahul Chatterjee          IT
+18      EMP019        Aditi Bose       Sales
+19      EMP020     Aniket Kapoor          HR
+20      EMP021          Sai Bhat       Sales
+'''
 
 #Average Age, Experience & Projects
-#print(df[["Age", "Experience", "Projects"]].mean())
+print(df[["Age", "Experience", "Projects"]].mean())
+''' Output:
+Age           39.400000
+Experience     9.183333
+Projects       6.950000
+dtype: float64'''
 
 #Try value_counts() on Department
 print(df["Department"].value_counts()) #it gives the count of each unique value present in the colunmn.
+''' Output:
+Department
+Marketing     23
+Sales         22
+Operations    22
+Finance       19
+IT            19
+HR            15
+Name: count, dtype: int64
+'''
